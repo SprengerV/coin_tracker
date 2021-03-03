b8f34cb481c7afb28bd693d1427478450e48c5d0
Brendan Francis <B.Francis@SprengerV.com>
2021-03-03 07:48:58 +0000

    init, update, and balance all working

--- cointrack.js
+++ cointrack.js
@@ -1,16 +1,21 @@
-const init = require('./lib/init.js');
-const update = require('./lib/update.js');
-const balance = require('./lib/balance.js');
+const path = require('path');
+const init = require(path.join(__dirname, 'lib', 'init.js'));
+const update = require(path.join(__dirname, 'lib', 'update.js'));
+const balance = require(path.join(__dirname, 'lib', 'balance.js'));
+const ask = require(path.join(__dirname, 'lib', 'ask.js'));
 
 const func = process.argv[2];
-const coin = process.argv[3].toLowerCase();
-const help = `\n"cointrack init %COIN%" to begin tracking that coin.
-"cointrack update %COIN%" to update a coin you\'re already tracking.
-"countrack balance %COIN%" to see the running totals for a coin you\'re tracking.`
+const coin = process.argv[3];
+const help = `
+cointrack init %COIN%                         Begin tracking that coin.
+cointrack update %COIN%                       Update a coin you're already tracking.
+countrack balance %COIN%                      See the running totals for a coin you're tracking.
+`
 
-if(!func) console.log(`You must pass additional arguments. ${help}`)
-else if(func == 'init') init(coin)
-else if(func == 'update') update(coin)
-else if(func == 'balance') balance(coin)
-else if(func == 'help') console.log(help)
+//if (!func) console.log('You must pass additional arguments.', help);
+if (!func) ask();
+else if (func === 'init') init(coin.toLowerCase());
+else if (func === 'update') update(coin);
+else if (func === 'balance') balance(coin);
+else if (func === 'help') console.log(help);
 else console.log(`Error: Invalid argument(s). ${help}`);
